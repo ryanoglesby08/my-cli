8b2eb2530dbed5d365899a0d04409d07258f8ea7
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-21 16:11:47 -0500

    add recommendation for VS Code to highlight command

--- my.js
+++ my.js
@@ -1,36 +1,49 @@
 #!/usr/bin/env node
 
 const { exec, execSync, spawn } = require('child_process');
-const fs = require('fs');
 const program = require('commander');
+const emoji = require('node-emoji');
 
 const withIO = process => {
-  process.stdout.on('data', (data) => {
+  process.stdout.on('data', data => {
     console.log(String(data));
   });
 
-  process.stderr.on('data', (data) => {
+  process.stderr.on('data', data => {
     console.error(String(data));
   });
 
-  return process
-}
-
-program
-  .version('0.1.0');
+  return process;
+};
 
 program
   .command('highlight <syntax>')
-  .description('apply syntax highlighting to a copied code snippet')
+  .description('Apply syntax highlighting to a copied code snippet')
   .action(syntax => {
-    exec('which highlight', (error, stdout, stderr) => {
-      if( error ) {
-        console.error("You don't seem to have `highlight` installed. Install it with `brew install highlight` then try again.");
+    exec('which highlight', error => {
+      if (error) {
+        console.error(
+          "You don't seem to have `highlight` installed. Install it with `brew install highlight` then try again."
+        );
         return;
       }
 
-      execSync(`pbpaste | highlight -O rtf --syntax=${syntax} --style=github --font-size=24 --font=Inconsolata | pbcopy`);
-      console.log('Code highlighted and in your clipboard!');
+      execSync(
+        `pbpaste | highlight -O rtf --syntax=${syntax} --style=github --font-size=24 --font=Inconsolata | pbcopy`
+      );
+
+      console.log(
+        `${emoji.get(
+          'lower_left_crayon'
+        )}  Code highlighted and in your clipboard!`
+      );
+      console.log(
+        `${emoji.get(
+          'point_right'
+        )} I recommend you copy code snippets out of Visual Studio Code for better highlighting though. ${emoji.get(
+          'point_left'
+        )}`
+      );
     });
   });
 
@@ -45,7 +58,7 @@ program
       spawn('./node_modules/.bin/http-server', [directory, '-p', port])
     );
 
-    httpServer.on('close', (code) => {
+    httpServer.on('close', code => {
       console.log(`HTTP server exited with code ${code}`);
     });
   });
@@ -55,24 +68,34 @@ program
   .description('backup important files to an external hard drive')
   .action(() => {
     exec('ls /Volumes | grep RhinoDrive', (error, stdout, stderr) => {
-      if( !stdout ) {
-        console.error("Connect the external hard drive and try again.");
+      if (!stdout) {
+        console.error('Connect the external hard drive and try again.');
         return;
       }
 
-      const sources = [`${process.env.HOME}/Projects`, `${process.env.HOME}/Documents`]
-      const dest = '/Volumes/RhinoDrive/Backup/'
+      const sources = [
+        `${process.env.HOME}/Projects`,
+        `${process.env.HOME}/Documents`,
+      ];
+      const dest = '/Volumes/RhinoDrive/Backup/';
 
       sources.forEach(source => {
         const sync = withIO(
-          spawn('rsync', ['--archive', '--verbose', '--delete', '--delete-excluded', '--exclude-from=backup-excludes', source, dest])
+          spawn('rsync', [
+            '--archive',
+            '--verbose',
+            '--delete',
+            '--delete-excluded',
+            '--exclude-from=backup-excludes',
+            source,
+            dest,
+          ])
         );
 
         sync.on('close', code => {
-          if( code === 0 ) {
+          if (code === 0) {
             console.log(`${source} copied to ${dest}`);
-          }
-          else {
+          } else {
             console.error(`Error: ${source} was not copied to ${dest}`);
           }
 
